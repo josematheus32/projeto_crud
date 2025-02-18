cbcca3e775961aba62cc37bc597d341d604cad2b
José Matheus <jose.sena27@aluno.ce.gov.br>
2025-02-18 07:46:45 -0300

    backend

---
+++ backend/produtos/src/test/java/br/com/projeto_crud/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto_crud.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
