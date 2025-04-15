214c967cb0b3f62056019fe7424c393b33350acc
José Matheus <jose.sena27@aluno.ce.gov.br>
2025-04-15 11:40:58 -0300

    Estilização e visibilidade dos botões

--- frontend/front/src/Formulario.js
+++ frontend/front/src/Formulario.js
@@ -1,17 +1,22 @@
-function Formulario(){
+function Formulario(botao){
     return (
-        <form className="container-form">
+        <form>
             <h1>Formulário</h1>
 
-            <input className="entrada" type="text" placeholder="Nome"/>
-            <input className="entrada" type="text" placeholder="Marca"/>
+            <input className="form-control" type="text" placeholder="Nome"/>
+            <input className="form-control" type="text" placeholder="Marca"/>
 
-            <div className="container-btn">
-            <input className="btn btn-success" type="button" value="Cadastrar"/>
-            <input className="btn btn-dark" type="button" value="Alterar"/>
-            <input className="btn btn-danger" type="button" value="Remover"/>
-            <input className="btn btn-warning" type="button" value="Cancelar"/>
-            </div>
+            {
+                botao
+                ?
+                <input className="btn btn-success" type="button" value="Cadastrar"/>
+                :
+                <div>
+                <input className="btn btn-dark" type="button" value="Alterar"/>
+                <input className="btn btn-danger" type="button" value="Remover"/>
+                <input className="btn btn-warning" type="button" value="Cancelar"/>
+                </div>
+            }
         </form>
     )
 }
