0e99498791962197096a76e0fe3b6d11d3739fa9
Marina <marina.lopes4@aluno.ce.gov.br>
2025-04-29 08:50:17 -0300

    Front conectado ao back

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -1,16 +1,24 @@
-import { useState } from 'react';
-import './App.css';
-import Formulario from './Formulario';
-import Tabela from './Tabela';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Formulario from "./Formulario";
+import Tabela from "./Tabela";
 
 function App() {
+  const [btnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
 
-  const[btnCadastrar] =useState(false);
+  //UseEffect
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+      .then((retorno) => retorno.json())
+      .then((retorno_convertido) => setProdutos(retorno_convertido));
+  }, []);
 
   return (
     <div>
-      <Formulario botao={btnCadastrar}/>
-      <Tabela/>
+      <p>{JSON.stringify(produtos)}</p>
+      <Formulario botao={btnCadastrar} />
+      <Tabela />
     </div>
   );
 }
